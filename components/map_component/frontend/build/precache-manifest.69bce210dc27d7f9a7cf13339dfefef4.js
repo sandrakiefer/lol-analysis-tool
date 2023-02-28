a5d12adf913588620bef265f880375c26b74ce32
Ferdinand Meyer <ferdinand.meyer.wi@gmail.com>
2023-02-28 18:02:43 +0100

    upload build part 2

---
+++ components/map_component/frontend/build/precache-manifest.69bce210dc27d7f9a7cf13339dfefef4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c337b7e217e0acb5a4624eca46db7e9e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d9b6a1259b3a3cd877b1",
+    "url": "./static/js/2.4a98c830.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.4a98c830.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "670bad225cd7e14f7cb2",
+    "url": "./static/js/main.3b6e24c3.chunk.js"
+  },
+  {
+    "revision": "c90b9d24986a0125bdff",
+    "url": "./static/js/runtime-main.24d5033d.js"
+  }
+]);
